a05fd3a047e41a7871d6c9d73e4ca156c5a77833
castorm <theiliyastorm@gmail.com>
2022-10-31 11:51:19 -0700

    sakht jadval
    sakht jadval 5 * 5

--- README.md
+++
@@ -1,2 +0,0 @@
-# PROJET1
- SED ALI
--- index.php
+++ index.php
@@ -1,7 +1,35 @@
-<?php
-$time=55 ;
-for ($A=0; $A<=$time; $A++) {
-    echo $A ;
+<html>
+<head>
+
+    <title></title>
+</head>
+<body>
+
+<table border="6">
+
+    <?php
+    $a = 1;
+    $b = 1;
+    $n = 5;
+    while ($a <= $n) {
+        echo "<tr>";
+        while ($b <= $n) {
+            echo "<td>";
+            echo $a * $b;
+            $b++;
+            echo "</td>";
+        }
+        $a++;
+
+        $b = 1;
+
+        echo "</tr>";
+    }
+    ?>
+
+</table>
+</body>
+</html>
 
 
-}
+
