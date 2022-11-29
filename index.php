7e2802204ad1d4e0ffe09b28e8ed0beef1e6b848
castorm <theiliyastorm@gmail.com>
2022-11-29 08:57:47 -0800

    جدول ضرب
    نوشتن کد جدول ضرب برای دریافت سطر و ستون با فورم

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
 <html>
 <head>
-
     <title></title>
 </head>
 <body>
-
 <table border="6">
-
     <?php
-    $a = 1;
-    $b = 1;
-    $n = 5;
-    while ($a <= $n) {
-        echo "<tr>";
-        while ($b <= $n) {
-            echo "<td>";
-            echo $a * $b;
-            $b++;
-            echo "</td>";
-        }
-        $a++;
-
+    if (isset($_POST['submit'])) {
+        $r = $_POST['row'];
+        $c = $_POST['col'];
+        $a = 1;
         $b = 1;
-
-        echo "</tr>";
+        while ($a <= $r) {
+            echo "<tr>";
+            while ($b <= $c) {
+                echo "<td>";
+                echo $a * $b;
+                $b++;
+                echo "</td>";
+            }
+            $a++;
+            $b = 1;
+            echo "</tr>";
+        }
     }
     ?>
-
 </table>
+<form method="post">
+    <label>rows:</label> <br/>
+    <input type="number" name="row"/><br/>
+    <label>columns:</label> <br/>
+    <input type="number" name="col"/><br/>
+    <input type="submit" name="submit" value="run"/>
+</form>
 </body>
 </html>
 
